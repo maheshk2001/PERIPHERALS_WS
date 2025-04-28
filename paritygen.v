10331c4b033c3ac1ee4a383f9b8d64dacc7a09e2
maheshk2001 <srihasam.kaushik@gmail.com>
2025-04-28 14:29:12 -0700

    Update paritygen.v
    Counter equality Value changed.

--- paritygen.v
+++ paritygen.v
@@ -7,7 +7,7 @@ module paritygen(ip,clk,rst_p,parity);
 	
 	always @(posedge clk or posedge rst_p)
 	begin
-		if (rst_p or counter == 32) // When reset is asserted or 32 bits counted we set the reg to default values.
+		if (rst_p or counter == 33) // When reset is asserted or 32 bits counted we set the reg to default values.
 		begin
 			parity <= 1'b0;
 			counter <= 0;
