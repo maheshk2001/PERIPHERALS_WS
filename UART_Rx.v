73fdafcf6c64d85e4092dae70c2c53a5692af7cc
jadhav-vishal <48425616+jadhav-vishal@users.noreply.github.com>
2025-05-15 13:30:10 -0700

    Update UART_Rx.v

--- UART_Rx.v
+++ UART_Rx.v
@@ -1,6 +1,6 @@
 module UART_Rx(DataOut, DoneRx, SerialInputData, CLK_Baudin, RstRx, Flag_Rx, DoneTx); // Module of UART Receiver.
 
-	parameter IDLE = 2'b00, RECEIVE = 2'b01, PARITY = 2'b10; // Coding the states for easy access.
+	parameter IDLE = 2'b00, RECEIVE = 2'b01, STOP = 2'b10; // Coding the states for easy access.
 	parameter size = 32; // No of Data Bits;
 	
 	input SerialInputData; // Data that comes as a input to the block.
@@ -18,7 +18,7 @@ module UART_Rx(DataOut, DoneRx, SerialInputData, CLK_Baudin, RstRx, Flag_Rx, Don
 	
 	wire Parity_Rx;
 	
-  	paritygen parity2 (.ip(shift_reg[0]),
+  paritygen parity2 (.ip(SerialInputData),
 						.clk(CLK_Baudin),
 						.rst_p(Paritygen_rstR),
 						.parity(Parity_Rx)); // Instantiating the parity generator module for parity bit generation. 
@@ -31,7 +31,7 @@ module UART_Rx(DataOut, DoneRx, SerialInputData, CLK_Baudin, RstRx, Flag_Rx, Don
 			shift_reg <= {size{1'b0}};
 			counter <= 0;
 			STATE <= IDLE;
-			//DoneRx <= 1'b0;
+			DoneRx <= 1'b0;
 			Flag_Rx <= 1'b0;
 			Paritygen_rstR <= 1'b1;
 		end
@@ -42,71 +42,87 @@ module UART_Rx(DataOut, DoneRx, SerialInputData, CLK_Baudin, RstRx, Flag_Rx, Don
 			IDLE : begin // IDLE state definition is similar to reset. We remain in IDLE until we detect the start bit(1'b0).
 				
 				shift_reg <= {size{1'b0}};
-				//DataOut <= shift_reg; // Do not clear DataOut in IDLE
-				counter <= 0;
+				DataOut <= shift_reg; // Do not clear DataOut in IDLE
+				counter <= 1'b0;
 				Flag_Rx <= 1'b0;
 				Paritygen_rstR <= 1'b1;
+                DoneRx <=1'b0;
+                
 				
 				if(SerialInputData == 1'b0) // If start bit is found
-				begin
+                  begin
 					STATE <= RECEIVE;
 					Paritygen_rstR <= 1'b0;
 					DoneRx <= 1'b0; // Clear DoneRx on new reception
-					counter <= counter + 1;
+					counter <= 1'b1;
+                    Flag_Rx <= 1'b0;
 				end
 				else begin
 					STATE <= IDLE;
-					counter <= 0;
+					counter <= 1'b0;
 				end
 			end
 			
 			RECEIVE : begin // Receiving the Data because we found the start bit.
-				if(counter !=0 & counter <= size) begin
-					shift_reg <= {SerialInputData, shift_reg[size-1:1]};
-					counter <= counter + 1;
-					STATE <= RECEIVE;
+              if(counter <= size) begin
+                shift_reg <= {SerialInputData, shift_reg[size-1:1]};
+                //shift_reg[counter-1] <= SerialInputData;
+				counter <= counter + 1;
+				//STATE <= RECEIVE;
+                //DoneRx <= 1'b0;
+                //Flag_Rx <= 1'b0;
+                //Paritygen_rstR <= 1'b0;
                    
 				end
-				else if(counter == size+1) begin
-                    if(Parity_Rx != SerialInputData) begin
+			  else if(counter == size+1) begin
+                    $display("[RX] Checking parity: calculated=%b, received=%b at time %0t", Parity_Rx, SerialInputData, $time);
+                   if(Parity_Rx != SerialInputData) begin
 						Flag_Rx <= 1'b1; // Request for retransmission
 						shift_reg <={size{1'b0}}; //flushing the reg if there is a mismatch
                         DataOut <= shift_reg;
 						STATE <= IDLE;
+                        DoneRx <=0;
+                        counter <= 1'b0;
 				    end
-				    else begin
+				   else begin
 						Flag_Rx <= 1'b0;
 						DataOut <= shift_reg;
-						STATE <= PARITY;
-                        //$display("T=%0t STATE=%b counter=%d shift_reg=%h DataOut=%h DoneRx=%b SerialInputData=%b Parity_Rx=%b", $time, STATE, counter, shift_reg, DataOut, DoneRx, SerialInputData, Parity_Rx);
-				    end
-                	DataOut <= shift_reg;
-                  	STATE <= PARITY;
-					
+						STATE <= STOP;
+                        counter <= counter +1;
+                        //Paritygen_rstR <= 1'b0;
+                        //DoneRx <= 1'b0;
+                    end
 				end
 			end
 			
 			
-			PARITY: begin
-				if(SerialInputData == 1'b1) begin
-					STATE <= IDLE;
-                    DataOut <= shift_reg;
-					DoneRx <= 1'b1; // Latch DoneRx when data is received
-					counter <= counter+1;
+			STOP: begin
+              if(counter == size +2) begin
+                STATE <= IDLE;
+                //DataOut <= shift_reg;
+				DoneRx <= 1'b1; // Latch DoneRx when data is received
+				//counter <= counter+1;
+                Flag_Rx <= 1'b0;
                     //$display("[RX] Stop bit detected, DoneRx set HIGH at time %0t\", $time);
-				end else begin
-                    DoneRx <= 0;
-                end
+			  end 
+              else begin
+                counter <= counter+1;
+                  
+              end
 			end
 			default : begin
 				STATE <= IDLE;
 			end
 			endcase
+        end
+    end
 			
 			// Clear Flag_Rx only when TX acknowledges retransmission
-			if (Flag_Rx && DoneTx) begin
+          always @(posedge CLK_Baudin) begin
+          	if (Flag_Rx && DoneTx) begin
 				Flag_Rx <= 1'b0;
-			end
-		end
-	end
+            end
+		  end
+		
+	
 endmodule
